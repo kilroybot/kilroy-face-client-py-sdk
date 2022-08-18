09908d57cc6d949aac514c1a2e1b603fe195397c
Sebastian Pietras <01133337@pw.edu.pl>
2022-08-18 15:58:30 +0200

    Moved to `gRPC` (#12)
    Moved to gRPC

--- kilroy_face_client_py_sdk/src/kilroy_face_client_py_sdk/__init__.py
+++ kilroy_face_client_py_sdk/src/kilroy_face_client_py_sdk/__init__.py
@@ -1,23 +1,5 @@
-from kilroy_face_client_py_sdk.client import FaceClient
-from kilroy_face_py_shared import (
-    JSON,
-    JSONSchema,
-    PostSchema,
-    StatusEnum,
-    Status,
-    StatusNotification,
-    Config,
-    ConfigSchema,
-    ConfigNotification,
-    ConfigSetRequest,
-    ConfigSetReply,
-    PostRequest,
-    PostReply,
-    ScoreRequest,
-    ScoreReply,
-    ScrapRequest,
-    ScrapReply,
-)
+from kilroy_face_py_shared import *
+from kilroy_face_client_py_sdk.client import FaceServiceStub, FaceService
 from kilroy_face_client_py_sdk.resources import (
     resource,
     resource_bytes,
